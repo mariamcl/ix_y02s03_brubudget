eed1e1617f1504241310ae316cf7f6600f8fad0b
Maria McLaughlin <mariamclaughlin@college.harvard.edu>
2015-07-27 16:15:01 +0200

    added association between user and expenditure

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -1,25 +1,42 @@
 class ExpendituresController < ApplicationController
-
+before_filter :authenticate_user!
   def index
-    @expenditures = Expenditure.all.order(purchase_date: :desc)
+    @expenditures = Expenditure.where(user_id: current_user.id)
   end
 
   def new
-    @expenditure = Expenditure.new
+    if user_signed_in?
+      @expenditure = Expenditure.new
+    else
+      redirect_to new_user_session_path
+      flash.notice = "You need to sign in first"
+    end
   end
 
   def create
-    @expenditure = Expenditure.new(expenditure_params)
+    @expenditure = current_user.expenditures.build expenditure_params
     if @expenditure.save
       redirect_to expenditure_path(@expenditure)
     else
-      redirect_to new_expenditure_path
+      render 'new'
     end
   end
 
   def show
     @expenditure = Expenditure.find(params[:id])
   end
+
+  def destroy
+    @expenditure = Expenditure.find params[:id]
+    if @expenditure.user = current_user
+      @expenditure.destroy
+      redirect_to expenditure_path
+      flash.notice = "Successful"
+    else
+      redirect_to expenditure_path
+    end
+  end
+
 
   private
     def expenditure_params
